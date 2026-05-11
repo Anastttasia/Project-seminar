370292fe4ebf731bcd4c3222066ed076fdfffc0f
Anastasia <nastya.kaneva.99@gmail.com>
2026-05-11 18:36:24 +0300

    add type sports on main page

--- rentService/rentplayground/views.py
+++ rentService/rentplayground/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from sports.SportTypes import Sport, SPORT_TYPES
+
+CURRENT_SPORTS = [
+    Sport(SPORT_TYPES[0], 'Футбол'),
+    Sport(SPORT_TYPES[1], 'Баскетбол'),
+    Sport(SPORT_TYPES[2], 'Волейбол'),
+    Sport(SPORT_TYPES[3], 'Настольный теннис'),
+]
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", context={"sports": CURRENT_SPORTS})
